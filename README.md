79382dd9e13753778d7203e3390cd69fa38a7dec
Matthieu Buffet <mtth.bfft@gmail.com>
2019-03-17 10:22:52 -0700

    Fix errors in problem statement, add context info

--- README.md
+++ README.md
@@ -1,23 +1,61 @@
 # Win32k mitigation
 
-This project is just a test case to try the new win32k.sys system call filtering mitigation in Windows 10.
+This project is just a test case to try the new win32k.sys system call filtering mitigation in Windows 8.
 
-The win32k mitigation policy is a per-thread mitigation which, if enabled, denies a process the right to call most graphics-related system call within wink32.sys. There are way too many of them (~1000, compared to the kernel's 400 ones), and most of their implementations is old code, making it a target for vulnerability researchers.
+The win32k mitigation policy is a per-thread mitigation which, if enabled, denies the thread the right to call most graphics-related system call within `wink32.sys`. There are way too many of them (~1000, compared to the kernel's 400 ones), and most of their implementations is old code, so this mitigation is quite important since it blocks a high-value target for vulnerability researchers. Unfortunately, only Microsoft Edge and Chrome use it as part of their sandbox, because there is no supported way to use it.
 
-The first thing you have to get around when enabling it is that minimal applications (C console applications compiled with msvc and just a `#include <Windows.h>`) won't start anymore. `CreateProcess()` returns an error, and a troubling 0xC0000142 NT status ([STATUS_DLL_INIT_FAILED](https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/596a1078-e883-4972-9bbc-49e60bebca55)) is shown to the user in a dialog box:
+# Filtering mechanism
+
+When a thread first calls a graphics syscall, it gets converted to a "GUI thread". This conversion happens once, and so that's where the mitigation ended up being implemented, probably for performance considerations. The problem is, enforcing the mitigation on an existing process leaves room for many threads to already be converted and thus allowed to make arbitrary system calls.
+
+The mitigation can be enabled in one of two ways :
+  - calling SetProcessMitigationPolicy(ProcessSystemCallDisablePolicy) with a handle to an existing process. This can only be really effective for that process' child processes, as explained earlier;
+  - calling CreateProcess(EXTENDED_STARTUPINFO_PRESENT) with a `PROC_THREAD_ATTRIBUTE_LIST` containing a `PROC_THREAD_ATTRIBUTE_MITIGATION_POLICY` attribute. This will necessarily affect all the threads.
+
+# Compatibility issues
+
+When enabled, the mitigation only allows startup of small programs compiled with no graphics imports and linked for the Console subsystem.
+
+However, when linked against a graphics library (say, `user32.dll`), `CreateProcess()` returns an error, and a message box shows an unfriendly `0xC0000142` NT status to the user ([STATUS_DLL_INIT_FAILED](https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/596a1078-e883-4972-9bbc-49e60bebca55)) :
 
 ![Dialog box with status 0xC0000142](https://github.com/mtth-bfft/win32k-mitigation/raw/master/docs/img/start_error_c0000142.png)
 
-To be continued
+A detour in WinDBG shows that, right after it loaded `imm32.dll` (as a dependency of user32.dll), `ntdll!_LdrpInitialize()` called `ntdll!LdrpInitializationFailure()` which, in turn, called `NtRaiseHardError()` with that status code.
+
+Since debugging processes during their startup (when nothing has been initialized yet) is hard, I tried importing all these graphics library at runtime (using `LoadLibrary`) instead of load time (using an import in the PE header). Results are a bit clearer:
+
+```
+ [.] Running from Z:\win32k-mitigation.exe
+ [.] Child process created: pid 5000
+ [.] Child process started successfully
+ [+] Child running with filtered Win32k syscalls
+
+ [.] Trying all gdi32full dependencies:
+ [!] Unable to load GDI32.dll: code 8
+ [!] Unable to load USER32.dll: code 8
+
+ [.] Trying all user32 dependencies:
+ [!] Unable to load GDI32.dll: code 8
+
+ [.] Trying all gdi32 dependencies:
+ [!] Unable to load api-ms-win-gdi-internal-uap-l1-1-0.dll: code 8
+
+ [+] Child exiting successfully
+ [.] Child process exited with code 0x00000000
+ [.] All done, return code 0
+```
+
+With runtime loading, we can see exactly at which loading step `LoadLibrary()` fails (with a `ERROR_NOT_ENOUGH_MEMORY` code, this time (?)). It seems loading any one of user32.dll or gdi32.dll will fail because of `api-ms-win-gdi-internal-uap-l1-1-0.dll`. This is not an actual DLL name, but an [ApiSet](https://docs.microsoft.com/en-us/windows/desktop/apiindex/windows-apisets), so we have to get back to runtime debugging to sort it out. To be continued... 
 
 # TODO
 
-- Find out which system calls exactly are still allowed with this mitigation enforced
+- Find out whether SetProcessMitigationPolicy() actually enforces the restriction flag on all existing threads, and where
+- Find out if there is a way to generically stub win32k system calls at DLL load time
+- Automate (e.g. JS WinDBG script) listing filtered/non-filtered Win32k syscalls
 
 # References
 
 - [MSDN documentation on SetProcessMitigationPolicy](https://docs.microsoft.com/en-us/windows/desktop/api/processthreadsapi/nf-processthreadsapi-setprocessmitigationpolicy)
 - [MSDN documentation on the system call disable policy](https://docs.microsoft.com/en-us/windows/desktop/api/winnt/ns-winnt-process_mitigation_system_call_disable_policy)
-- [The Windows Sandbox Paradox - James Forshaw Nullcon 2015](https://nullcon.net/website/archives/ppt/goa-15/the-windows-sandbox-paradox.pdf)
-- [Reverse Engineering the Win32k Type Isolation Mitigation - Quarkslab](https://blog.quarkslab.com/reverse-engineering-the-win32k-type-isolation-mitigation.html)
 - [Win32k System Call Filtering Deep Dive - Morten Schenk](https://improsec.com/tech-blog/win32k-system-call-filtering-deep-dive)
+- [The Windows Sandbox Paradox - James Forshaw Nullcon 2015](https://nullcon.net/website/archives/ppt/goa-15/the-windows-sandbox-paradox.pdf)
--- src/main.c
+++ src/main.c
@@ -3,24 +3,159 @@
 #include <stdio.h>
 #include "mem.h"
 
+static BOOL TestLoadLib(PCTSTR swzName)
+{
+	if (LoadLibrary(swzName) == NULL)
+	{
+		_tprintf(TEXT(" [!] Unable to load %s: code %u\n"), swzName, GetLastError());
+		return FALSE;
+	}
+	return TRUE;
+}
+
 int _tmain(int argc, PCTSTR argv[])
 {
 	int res = 0;
-	HWINSTA hWinsta = NULL;
 	HMODULE hSelf = NULL;
 	TCHAR swzSelfPath[MAX_PATH + 1] = { 0 };
-	HMODULE hWin32u = NULL;
-	PVOID pNtUserSetImeInfoEx = NULL;
 	PPROC_THREAD_ATTRIBUTE_LIST pAttr = NULL;
 	SIZE_T dwBufLen = 0;
 	DWORD dwMitigationPolicy = PROCESS_CREATION_MITIGATION_POLICY_WIN32K_SYSTEM_CALL_DISABLE_ALWAYS_ON;
 	STARTUPINFOEX startInfo = { 0 };
 	PROCESS_INFORMATION procInfo = { 0 };
 	DWORD dwExitCode = 0;
+	PROCESS_MITIGATION_SYSTEM_CALL_DISABLE_POLICY effectivePolicy = { 0 };
 
 	if (argc >= 1 && _tcsicmp(argv[0], TEXT("self-run")) == 0)
 	{
 		_tprintf(TEXT(" [.] Child process started successfully\n"));
+
+		if (!GetProcessMitigationPolicy(GetCurrentProcess(), ProcessSystemCallDisablePolicy, &effectivePolicy, sizeof(effectivePolicy)))
+		{
+			_tprintf(TEXT(" [!] Could not query system call filter policy in child: code %u\n"), GetLastError());
+		}
+		else if (!effectivePolicy.DisallowWin32kSystemCalls)
+		{
+			_tprintf(TEXT(" [!] Child running with no filtering on Win32k syscalls\n"));
+		}
+		else
+		{
+			_tprintf(TEXT(" [+] Child running with filtered Win32k syscalls\n"));
+		}
+
+		if (LoadLibrary(TEXT("gdi32full.dll")) == NULL)
+		{
+			_tprintf(TEXT("\n"));
+			_tprintf(TEXT(" [.] Trying all gdi32full dependencies:\n"));
+			TestLoadLib(TEXT("msvcp_win.dll"));
+			TestLoadLib(TEXT("api-ms-win-crt-string-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-crt-runtime-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-crt-private-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-string-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-localization-l1-2-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-heap-l2-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-rtlsupport-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-libraryloader-l1-2-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-sysinfo-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-memory-l1-1-1.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-errorhandling-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-processenvironment-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-file-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-handle-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-registry-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-file-l1-2-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-synch-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-heap-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-file-l2-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-memory-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-threadpool-l1-2-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-processthreads-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-debug-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-string-l2-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-security-base-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-processthreads-l1-1-1.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-profile-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-interlocked-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-kernel32-legacy-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-heap-obsolete-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-string-obsolete-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-stringansi-l1-1-0.dll"));
+			TestLoadLib(TEXT("ntdll.dll"));
+			TestLoadLib(TEXT("win32u.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-delayload-l1-1-1.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-delayload-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-privateprofile-l1-1-0.dll"));
+			TestLoadLib(TEXT("api-ms-win-core-localization-private-l1-1-0.dll"));
+			TestLoadLib(TEXT("GDI32.dll"));
+			TestLoadLib(TEXT("USER32.dll"));
+			_tprintf(TEXT("\n"));
+
+			if (LoadLibrary(TEXT("USER32.dll")) == NULL)
+			{
+				_tprintf(TEXT("\n"));
+				_tprintf(TEXT(" [.] Trying all user32 dependencies:\n"));
+				TestLoadLib(TEXT("win32u.dll"));
+				TestLoadLib(TEXT("ntdll.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-localization-l1-2-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-registry-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-heap-l2-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-libraryloader-l1-2-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-eventing-provider-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-processthreads-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-synch-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-string-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-sysinfo-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-security-base-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-handle-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-errorhandling-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-string-l2-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-synch-l1-2-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-processenvironment-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-file-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-processthreads-l1-1-1.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-memory-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-profile-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-heap-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-memory-l1-1-3.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-privateprofile-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-atoms-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-heap-obsolete-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-string-obsolete-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-localization-obsolete-l1-2-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-stringansi-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-sidebyside-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-kernel32-private-l1-1-0.dll"));
+				TestLoadLib(TEXT("KERNELBASE.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-kernel32-legacy-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-appinit-l1-1-0.dll"));
+				TestLoadLib(TEXT("GDI32.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-delayload-l1-1-1.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-delayload-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-apiquery-l1-1-0.dll"));
+				_tprintf(TEXT("\n"));
+			}
+
+			if (LoadLibrary(TEXT("GDI32.dll")) == NULL)
+			{
+				_tprintf(TEXT("\n"));
+				_tprintf(TEXT(" [.] Trying all gdi32 dependencies:\n"));
+				TestLoadLib(TEXT("ntdll.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-heap-l2-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-libraryloader-l1-2-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-processthreads-l1-1-1.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-processthreads-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-profile-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-sysinfo-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-errorhandling-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-gdi-internal-uap-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-delayload-l1-1-1.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-delayload-l1-1-0.dll"));
+				TestLoadLib(TEXT("api-ms-win-core-apiquery-l1-1-0.dll"));
+				_tprintf(TEXT("\n"));
+			}
+		}
+
+		_tprintf(TEXT(" [+] Child exiting successfully\n"));
 		ExitProcess(0);
 	}
 
@@ -28,32 +163,6 @@ int _tmain(int argc, PCTSTR argv[])
 	GetModuleFileName(hSelf, swzSelfPath, MAX_PATH);
 
 	_tprintf(TEXT(" [.] Running from %s\n"), swzSelfPath);
-
-	hWinsta = GetProcessWindowStation();
-	if (hWinsta == NULL)
-	{
-		res = GetLastError();
-		_ftprintf(stderr, TEXT("Error: unable to acquire handle to our own window station, code %u\n"), res);
-		goto cleanup;
-	}
-	_tprintf(TEXT(" [.] Window station handle: %p\n"), (PVOID)hWinsta);
-
-	hWin32u = LoadLibrary(TEXT("win32u.dll"));
-	if (hWin32u == NULL)
-	{
-		res = GetLastError();
-		_ftprintf(stderr, TEXT("Error: unable to load win32u.dll, code %u\n"), res);
-		goto cleanup;
-	}
-
-	pNtUserSetImeInfoEx = (PVOID)GetProcAddress(hWin32u, "NtUserSetImeInfoEx");
-	if (pNtUserSetImeInfoEx == NULL)
-	{
-		res = GetLastError();
-		_ftprintf(stderr, TEXT("Error: unable to locate NtUserSetImeInfoEx(), code %u\n"), res);
-		goto cleanup;
-	}
-	_tprintf(TEXT(" [.] NtUserSetImeInfoEx() is at %p\n"), pNtUserSetImeInfoEx);
 
 	if (InitializeProcThreadAttributeList(NULL, 1, 0, &dwBufLen) || GetLastError() != ERROR_INSUFFICIENT_BUFFER)
 	{
@@ -98,7 +207,7 @@ int _tmain(int argc, PCTSTR argv[])
 	}
 	else
 	{
-		_tprintf(TEXT(" [.] Child process exited with code %u\n"), dwExitCode);
+		_tprintf(TEXT(" [.] Child process exited with code 0x%08X\n"), dwExitCode);
 	}
 
 cleanup:
